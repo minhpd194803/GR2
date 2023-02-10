615d0a0954b1e90e3bc7908f39f4f1505cb282e1
fae1i <minh.pd194803@sis.hust.edu.vn>
2023-02-10 13:41:39 +0700

    api not working

---
+++ components/home/HomeScreen.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div>
+      GR2
+    </div>
+  </div>
+</template>
+
+<script>
+  import ElementUI from 'element-ui';
+  import axios from 'axios';
+
+  export default {
+    data(){
+      return {
+
+      }
+    },  
+    methods:{
+      printApi(){
+        axios('https://api.cosmos.network/blocks/latest', {
+          method: 'get', 
+          mode: 'no-cors',
+          headers: {
+            'Access-Control-Allow-Origin': 'http://localhost:3000/',
+          //   'Content-Type': 'application/json',
+          },
+        })
+          .then((response) => {
+            console.log(response)
+          })
+          .catch((response) => {
+            console.log(response)
+          })
+      },
+    },
+    mounted(){
+      this.printApi()
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <Tutorial/>
+  <HomeScreen/>
 </template>
 
 <script>
+  import HomeScreen from '../components/home/HomeScreen.vue'; 
   export default {
-    name: 'IndexPage'
+    name: 'IndexPage',
+    components: {
+      HomeScreen,
+    },
   }
 </script>
