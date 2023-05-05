bb36823be4683c16e97e6a864498cd65f7aa80db
minh.pd194803 <minh.pd194803@sis.hust.edu.vn>
2023-05-05 11:44:18 +0700

    create relayers

---
+++ pages/relayers/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <Navbar :active-index="'4'"/>
+    <ChainInfo :chainName="getChainName" @change-chain="setChainName"/>
+    {{ hehe }}
+    <Relayers/>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import Navbar from '../../components/Navbar/Navbar.vue';
+import ChainInfo from '../../components/home/ChainInfo.vue';
+import Relayers from '../../components/relayers/Relayers.vue';
+export default {
+  components: {
+    Relayers,
+    ChainInfo,
+    Navbar
+  },
+  data(){
+    return{
+      hehe: '',
+    }
+  },
+  computed: {
+    ...mapGetters(['getChainName']),
+  },
+  methods: {
+    setChainName(name){
+      this.$store.dispatch('setChainName', name)
+    },
+  },
+  mounted(){
+    
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
